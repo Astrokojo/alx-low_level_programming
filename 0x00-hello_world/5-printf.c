175258963422b724b76cbe0a604baa4a9affe0df
Astrokojo <astrokojo@gmail.com>
2023-07-13 18:05:11 -0700

    second commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - My program prints a phrase using the printf function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n);
+	return (0);
+}
