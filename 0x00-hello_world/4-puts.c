0d9c345ea67474b5e871cc0f2da7add60127b4ac
Astrokojo <astrokojo@gmail.com>
2023-07-13 17:34:05 -0700

    commitment 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - My program prints the phrase Programming is like building a multilingual puzzle using the puts function
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programing is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
