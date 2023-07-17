a9acd8ca467c4789a05293e3c0c7c7f6e2f6979e
Astrokojo <astrokojo@gmail.com>
2023-07-17 08:33:01 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints a sentence and a date, followed
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
