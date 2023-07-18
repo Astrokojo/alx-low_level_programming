ce5b8196095fddd6ea8d13f6cb5cd59a5cd32a1e
Astrokojo <astrokojo@gmail.com>
2023-07-18 16:23:32 -0700

    sleeeepy

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: an integer
+ *
+ * Return: 0 (Success)
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar(43);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+		_putchar(45);
+	}
+	else
+	{
+		return (0);
+		_putchar(48);
+	}
+}
