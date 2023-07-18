6c46e7bf71df9c6f5d157e656f263d90c9fb1c03
Astrokojo <astrokojo@gmail.com>
2023-07-18 09:18:45 -0700

    asd

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,11 @@
-int _putchar(int char);
+#include<unistd.h>
+#include<stdio.h>
+/**
+ * _putchar - does same as putchar
+ *
+ * Return: 0 (Success)
+ */
+void _putchar(int character)
+{
+	printf(int character);
+}
