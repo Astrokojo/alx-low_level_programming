2f1bf44b0b03eca7d1368e3dc8d753eb3922a429
Astrokojo <astrokojo@gmail.com>
2023-07-18 16:38:17 -0700

    ds

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ *_abs - Compute aboslute value
+ *
+ *@i: input
+ *Return: absolute val
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
