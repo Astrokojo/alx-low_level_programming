4a6cd848c4661625a0d693a154a042447f23c260
Astrokojo <astrokojo@gmail.com>
2023-07-18 14:05:51 -0700

    broke the putchar into individual chars

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,10 +3,17 @@
 /**
  * main - check the code
  *
- * Return: Always 0
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	_putchar("_putchar\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,1 @@
-#include<unistd.h>
-#include<stdio.h>
-/**
- * _putchar - does same as putchar
- *
- * Return: 0 (Success)
- */
-void _putchar(int character)
-{
-	printf(int character);
-}
+int _putchar(char c);
