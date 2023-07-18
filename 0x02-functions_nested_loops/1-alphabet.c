9c0b3e421fff48d00d06cc12133054068c01af5d
Astrokojo <astrokojo@gmail.com>
2023-07-18 07:21:19 -0700

    ss

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include<main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char az;
+	for(az = 'a' ;az < 'z' ; az ++)
+	{
+		_putchar(az);
+	}
+	return (0);
+}
