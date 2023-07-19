93501fe41df7a231f0ed8b312ab60ab7dfd50347
Astrokojo <astrokojo@gmail.com>
2023-07-18 17:51:02 -0700

    sad

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include"main.h"
+#include <studio.h>
+/**
+ * print_to_98 - like it says on the tin
+ *
+ * @n: input to start count at
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
