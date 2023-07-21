28c2ff4b236c58eae60ffe1bc77cc83286b9968f
Astrokojo <astrokojo@gmail.com>
2023-07-20 20:01:27 -0700

    fixed isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,16 +4,13 @@
  * _isupper - checks for uppercase character
  * @c: character to check
  *
- * Return: 1 if c is uppercase, 0 otherwise
+ * Return: 1 if c is uppercase, else 0
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z');
-  {
-            return (1);
-  }
-  else
-  {
-        return (0);
-  }
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
 }
