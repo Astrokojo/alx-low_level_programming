1b0653c0b9212b36343368a410b05ad9d260ba05
Astrokojo <astrokojo@gmail.com>
2023-07-25 09:24:46 -0700

    sdad

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,12 +2,19 @@
 
 /**
  * print_rev - prints the reverse of a string
+ *@s: param
  *
  * Return: Always 0.
  */
 void print_rev(char *s)
 {
-	while (*s)
-		_putchar(*s--);
+	int length = _strlen(s);
+	int i;
+
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
 	_putchar('\n');
 }
