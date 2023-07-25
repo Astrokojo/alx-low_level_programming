d3e8f44bfeb551933d00f34c58323c585303a365
Astrokojo <astrokojo@gmail.com>
2023-07-25 05:53:11 -0700

    second task

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,6 +3,8 @@ include "main.h"
 /**
  * swap_int- swaps the values of two integers
  *
+ *@a: first interger to be swapped
+ *@b: second integer to be swapped
  * Return: Always 0
  */
 void swap_int(int *a, int *b)
