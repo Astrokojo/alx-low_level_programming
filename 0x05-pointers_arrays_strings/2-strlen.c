4d46d70e3658eca8394330da5904d10fdbced796
Astrokojo <astrokojo@gmail.com>
2023-07-25 06:56:48 -0700

    third task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * _strlen - prints the length of a string
+ *
+ * @s: pointer variable
+ *
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+
+	return (len);
