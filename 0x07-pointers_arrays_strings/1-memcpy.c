2028560dc5af36e0ba65c1a1f3d324d2865ff15a
Astrokojo <astrokojo@gmail.com>
2023-08-01 18:18:43 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memcpy-does the same thing as memcpy
+ * @dest: pointer to dest
+ * @src: constant byte
+ * @n: bytes of the memory pointed to by dest
+ * Return: returns char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	while (n--)
+		dest[n] = src[n];
+	return (s);
+}
