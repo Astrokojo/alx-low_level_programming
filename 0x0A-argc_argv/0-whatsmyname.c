4375b26ceca12973541e31b028328d71e7d59d36
Astrokojo <astrokojo@gmail.com>
2023-08-07 01:16:31 -0700

    zeroth task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Main - Entry point
+ * @argc - count
+ * @argv - pointer
+ *
+ * Return - EXIT_SUCCESS
+ */
+int main (int argc, char *argv[])
+{
+	printf ("\.%s\\n", argv[0]);
+	return (0);
+}
