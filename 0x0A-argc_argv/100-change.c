dcb87fc2858a6331b34ece1c0adae73520e45262
Astrokojo <astrokojo@gmail.com>
2023-08-08 23:53:32 +0000

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,13 +1,48 @@
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 
 /**
- * main - Entry point
- * @argc: count
- * @argv: vector
+ * main - prints the minimum number of coins to make change for an amount of
+ * money, followed by a new line
  *
- * Return: 0 Success
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int
+	int i, cents, coins = 0;
+	int denominations[5];
+	int num_denominations;
+
+	/* check for correct number of arguments */
+	if (argc != 2)
+		return (printf("Error\n"), 1);
+
+	/* convert cents to integer */
+	cents = atoi(argv[1]);
+
+	/* check for negative cents */
+	if (cents < 0)
+		return (printf("0\n"), 0);
+
+	/* initialize denominations array */
+	denominations[0] = 25;
+	denominations[1] = 10;
+	denominations[2] = 5;
+	denominations[3] = 2;
+	denominations[4] = 1;
+
+	/* calculate the size of the array */
+	num_denominations = sizeof(denominations) / sizeof(int);
+
+	/* calculate the number of coins */
+	for (i = 0; i < num_denominations && cents; i++)
+	{
+		coins += cents / denominations[i];
+		cents %= denominations[i];
+	}
+
+	printf("%d\n", coins);
+	return (0);
+}
