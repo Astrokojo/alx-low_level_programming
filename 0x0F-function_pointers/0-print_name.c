a659e5c78ac2bea48ee7cc4162ff2551eb976f3f
Kojo Antwi Osei-Sarfo <astrokojo@gmail.com>
2023-08-16 16:04:31 -0700

    removed #include

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,9 @@
-#include "function_pointers.h"
-
 /**
  * print_name - function that prints a name
  *
  * @name: the name to print
  * @f: pointer to the print function
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
