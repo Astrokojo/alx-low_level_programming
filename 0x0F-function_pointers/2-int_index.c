c6dcbecb291d963330be97107cfbdf6d0444dcde
Kojo Antwi Osei-Sarfo <astrokojo@gmail.com>
2023-08-16 19:35:09 -0700

    updated int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,17 +4,19 @@
  * int_index - function searches for an integer
  *@array: array
  * @size: number of elems in arr
- * @cmp: pointer to function that does calue comparison
+ * @cmp: pointer to function that does value comparison
  * Return: index of first elem for which cmp fctn !return 0
  * if no elem matches return -1
  * if size <= 0, return -1
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
 	if (!array || size <= 0 || !cmp)
 		return (-1);
 
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
 			return (i);
