0135d83f71da2be2b75f987ac834b3fb6692a4e8
Kojo Antwi Osei-Sarfo <astrokojo@gmail.com>
2023-08-24 21:24:55 -0700

    added 3-node and updated 2_note

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,15 +1,16 @@
 #include "lists.h"
-#include <stdio.h>
+
 /**
- * add_node - a function that adds
- * a new node at the beginning of a list_t list.
+ * add_node_end- a function that adds
+ * a new node at the end of a list_t list.
  *
  * @head: pointer to new head
  * @str: string to duplicate
  * Return: address of new element, NULL if failed
  */
-size_t *add_node(list_t **head, const char *str)
+size_t *add_node_end(list_t **head, const char *str)
 {
+	list_t *last;
 	list_t *new;
 
 	new = malloc(sizeof(struct list_t));
@@ -19,7 +20,16 @@ size_t *add_node(list_t **head, const char *str)
 
 	new->str = strdup(str);
 	new->len = _strlen(len);
-	new->next = *head;
-	*head = new;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		last = *head;
+		while (last->next)
+			last = last->next;
+		last->next = new;
+	}
 	return (new);
 }
