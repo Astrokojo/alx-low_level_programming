fb1f67bd74e04f550ddf0f9cdc1b0132ed2084c3
Kojo Antwi Osei-Sarfo <astrokojo@gmail.com>
2023-08-29 06:39:42 -0700

    Added List Length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len- returns the number of elements of a linked listint_t list
+ * listint_len- returns the number of elements of a linked listint_t list
  * @h: pointer to head
  *
  * Return: The number of nodes
