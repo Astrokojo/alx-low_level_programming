e0e2468ac4aff33358d46f9546a6c066a761de55
Astrokojo <astrokojo@gmail.com>
2023-09-01 21:38:53 +0000

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,16 +9,19 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n;
+	int i;
 	
 	if (!b)
 		return (0);
-	while (*b != '\0')
+	
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (*b != '0' && *b != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		n = n * 2 + (*b - '0');
-		n++;
+		
+		n = (n << 1) | ([b[i] - '0');
 	}
+	
 	return (n);
 
 }
