f2233a8215c04a5bbdedfd96813361467dfa6593
Kojo Antwi Osei-Sarfo <astrokojo@gmail.com>
2023-09-04 18:21:39 -0700

    update: task 2 - fixed permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fildes = open(filename, O_WRONLY | O_CREAT | O_TRUNC | 0600);
+	fildes = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fildes == -1)
 		return (-1);
 
