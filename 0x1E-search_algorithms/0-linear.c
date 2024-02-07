06f5d76835679ecdc4dd33bba3021c5f266aaf08
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-02-07 13:23:00 +0000

    refactor: refactor code for file 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -8,6 +8,7 @@
  *
  * Return: first index where value is located else (-1)
  */
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
@@ -18,9 +19,9 @@ int linear_search(int *array, size_t size, int value)
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-
 		if (array[i] == value)
 			return (i);
 	}
+
 	return (-1);
 }
